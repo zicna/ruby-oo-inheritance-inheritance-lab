3b9854fd4cb9a1e82202dd9df096692e09d2b9be
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-01-18 22:57:54 -0600

    commit tree fe6d86309cf60a7ceef7da89bde97e4af96aa263

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def new(new)
+        new = Student.new
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
